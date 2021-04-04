f5085deeea7eb205f0d7e7757dfdb56fcd4f8f0a
aidanbxyz <43518094+aidanbxyz@users.noreply.github.com>
2021-04-04 15:54:45 -0500

    Update installlinux.sh

--- installlinux.sh
+++ installlinux.sh
@@ -3,11 +3,6 @@
 mv node_modules.a/ node_modules/
 apt install -y nano nodejs npm
 
-npm install random-useragent
-npm install system-sleep
-npm install got
-npm install ws
-
 echo "Just open spamkahoot.js with your favorite text editor and change the kahootCode variable to your kahoot game pin"
 echo "Also change kahootBotPrefix to whatever username you want"
 echo "Open terminal and navigate to this directory. Then type 'node spamkahoot.js'"
