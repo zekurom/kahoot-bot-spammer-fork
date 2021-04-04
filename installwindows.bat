8e910d629cbfd4bae00b987617de43f4b96aa1f3
aidanbxyz <43518094+aidanbxyz@users.noreply.github.com>
2021-04-04 15:54:32 -0500

    Update installwindows.bat

--- installwindows.bat
+++ installwindows.bat
@@ -4,10 +4,6 @@ cls
 powershell.exe -ExecutionPolicy Bypass -Command "& Invoke-WebRequest -Uri 'https://nodejs.org/dist/v14.16.0/node-v14.16.0-x64.msi' -OutFile 'installnode.msi'"
 installnode.msi /quiet /norestart
 rename node_modules.a node_modules
-npm install random-useragent
-npm install system-sleep
-npm install got
-npm install ws
 echo Just open spamkahoot.js with your favorite text editor and change the kahootCode variable to your kahoot game pin
 echo Also change kahootBotPrefix to whatever username you want
 echo Open cmd and navigate to this directory. Then type "node spamkahoot.js"
