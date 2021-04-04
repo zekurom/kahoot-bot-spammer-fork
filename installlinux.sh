e50f40e196f81379231292204d98fb3c31efdf6a
aidanbxyz <43518094+aidanbxyz@users.noreply.github.com>
2021-04-04 15:38:48 -0500

    linux install script

---
+++ installlinux.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mv node_modules.a/ node_modules/
+apt install -y nano nodejs npm
+
+echo "Just open spamkahoot.js with your favorite text editor and change the kahootCode variable to your kahoot game pin"
+echo "Also change kahootBotPrefix to whatever username you want"
+exit 0
